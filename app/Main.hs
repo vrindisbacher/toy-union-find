c10ef3df733b6dc6e52c3e3338726703f31717f1
vrindisbacher <vrindisbacher77@gmail.com>
2025-01-26 20:15:08 -0800

    restore other tests

--- app/Main.hs
+++ app/Main.hs
@@ -19,19 +19,19 @@ main = do
     res1 <- check e1
     print res1
 
-    -- let body = EAdd (EVar "x") (EInt 3)
-    -- let e2 = EApp (ELam "x" body) (EAdd (EInt 1) (EInt 2))
-    -- res2 <- check e2
-    -- print res2
+    let body = EAdd (EVar "x") (EInt 3)
+    let e2 = EApp (ELam "x" body) (EAdd (EInt 1) (EInt 2))
+    res2 <- check e2
+    print res2
 
-    -- let e3 = EBind "x" (EInt 3) (EAdd (EVar "x") (EInt 3))
-    -- res3 <- check e3
-    -- print res3
+    let e3 = EBind "x" (EInt 3) (EAdd (EVar "x") (EInt 3))
+    res3 <- check e3
+    print res3
 
-    -- let body' = EAdd (EVar "x") (EFloat 3)
-    -- let e4 = EApp (ELam "x" body') (EAdd (EFloat 1) (EFloat 2))
-    -- res4 <- check e4
-    -- print res4
+    let body' = EAdd (EVar "x") (EFloat 3)
+    let e4 = EApp (ELam "x" body') (EAdd (EFloat 1) (EFloat 2))
+    res4 <- check e4
+    print res4
 
     let f1' = ELam "x" (EAdd (EVar "x") (EInt 1))
     let f2' = ELam "f" (ELam "x" (EApp (EVar "f") (EVar "x")))
